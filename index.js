6cd018d2ce3fb007f0f7e70fa070b920eebfe9ed
alimajid <alidzk98@gmail.com>
2022-10-10 16:50:26 +0300

    Done !

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Follow along with the examples here
+function sayHello() {
+  console.log("Hello!");
+}
+function sayHelloToGuadalupe() {
+  console.log("Hello, Guadalupe!");
+}
+function sayHelloToLiz() {
+  console.log("Hello, Liz!");
+}
+function sayHelloToSamip() {
+  console.log("Hello, Samip!");
+}
+function sayHelloTo(firstName) {
+  console.log(`Hello, ${firstName}!`);
+}
+
+sayHelloTo("Ali");
